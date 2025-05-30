a5c1d5c4e26d430832ab84b3fb40fc5aa45b7004
Gabriel Cabral <gabriel.cabral.medeiros@ccc.ufcg.edu.br>
2025-05-30 00:41:42 -0300

    ajuste no settings

--- StudyLife_Partner/settings.py
+++ StudyLife_Partner/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-1uxm!l9uc9g@po)&g8za^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.railway.app', cast=Csv(), 'studylifepartner.pythonanywhere.com')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.railway.app', 'studylifepartner.pythonanywhere.com', cast=Csv())
 
 
 # Application definition
@@ -174,4 +174,3 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = True
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
